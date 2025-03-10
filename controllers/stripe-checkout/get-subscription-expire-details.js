8adfb44fffc2adb265c1488699ad466ee6ff212c
Sanchit Raghuwanshi <97170850+Sanchitraghu@users.noreply.github.com>
2025-03-10 16:44:48 +0530

    Fixed date issue in getting susbcription expiration data

--- controllers/stripe-checkout/get-subscription-expire-details.js
+++ controllers/stripe-checkout/get-subscription-expire-details.js
@@ -4,15 +4,22 @@ const getSubscriptionExpireDetails = async (req, res) => {
   const { uuid } = req.query;
   try {
     const user = await User.findOne({ uuid: uuid });
+    console.log(user);
     if (!!user) {
       return res.status(200).send({
-        subscriptionExpiresOn: user?.subscriptionExpiresOn,
+        subscriptionExpiresOn:
+          user?.subscriptionExpiresOn ||
+          new Date(new Date() - 30 * 24 * 60 * 60 * 1000),
       });
     }
-    return res.status(200).send({ subscriptionExpiresOn: new Date() - 30 * 24 * 60 * 60 * 1000 });
+    return res.status(200).send({
+      subscriptionExpiresOn: new Date(new Date() - 30 * 24 * 60 * 60 * 1000),
+    });
   } catch (err) {
     console.log(err.message);
-    res.status(200).send({ subscriptionExpiresOn: new Date() - 30 * 24 * 60 * 60 * 1000 });
+    res.status(200).send({
+      subscriptionExpiresOn: new Date(new Date() - 30 * 24 * 60 * 60 * 1000),
+    });
   }
 };
 
